5b254ed643fc59ae48e9c9482ce043958291d0b8
Evgeny Mudrichenko <evgeny.mudrichenko@gmail.com>
2026-02-23 14:03:25 +0300

    refactor: move security-error arguments and codes from user module to security module

---
+++ shared/foundation/core/security/src/commonMain/kotlin/io/github/mudrichenkoevgeny/shared/foundation/core/security/error/naming/SecurityErrorCodes.kt
@@ -0,0 +1,9 @@
+package io.github.mudrichenkoevgeny.shared.foundation.core.security.error.naming
+
+/**
+ * Security-specific error codes for API responses and backend logging, used to categorize failures.
+ */
+object SecurityErrorCodes {
+    const val AUTHENTICATION_CONFIRMATION_REQUIRED = "AUTHENTICATION_CONFIRMATION_REQUIRED"
+    const val PASSWORD_TOO_WEAK = "PASSWORD_TOO_WEAK"
+}
--- shared/foundation/feature/user/src/commonMain/kotlin/io/github/mudrichenkoevgeny/shared/foundation/feature/user/error/naming/UserErrorArgs.kt
+++ shared/foundation/feature/user/src/commonMain/kotlin/io/github/mudrichenkoevgeny/shared/foundation/feature/user/error/naming/UserErrorArgs.kt
@@ -5,12 +5,4 @@ package io.github.mudrichenkoevgeny.shared.foundation.feature.user.error.naming
  */
 object UserErrorArgs {
     const val USER_ID = "userId"
-    const val PASSWORD_MIN_LENGTH = "passwordMinLength"
-    const val PASSWORD_FAIL_TOO_SHORT = "passwordTooShort"
-    const val PASSWORD_FAIL_NO_LETTER = "passwordNoLetter"
-    const val PASSWORD_FAIL_NO_UPPERCASE = "passwordNoUpperCase"
-    const val PASSWORD_FAIL_NO_LOWERCASE = "passwordNoLowerCase"
-    const val PASSWORD_FAIL_NO_DIGIT = "passwordNoDigit"
-    const val PASSWORD_FAIL_NO_SPECIAL_CHAR = "passwordNoSpecialChar"
-    const val PASSWORD_FAIL_TOO_COMMON = "passwordTooCommon"
 }
